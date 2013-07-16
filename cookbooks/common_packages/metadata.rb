78c898a72083b768cd391e89bf57c1fde048b967
Chaim <chaim@intercomp.co.il>
2013-07-17 01:34:16 +0300

    cookbook for installing distrubution packages on server

---
+++ cookbooks/common_packages/metadata.rb
@@ -0,0 +1,7 @@
+name             'common_packages'
+maintainer       'chakatz'
+maintainer_email 'chaim@intercomp.co.il'
+license          'GNU Public License 3.0'
+description      'Installs/Configures common_packages'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
